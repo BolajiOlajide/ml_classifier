77d392d44902447f4775423350946e5869c728f5
Bolaji Olajide <cooproton@gmail.com>
2019-09-17 08:52:01 +0100

    complete classificiation

--- ml.py
+++ ml.py
@@ -1,5 +1,25 @@
+from sklearn import tree
 from sklearn.datasets import load_iris
 
 iris_dataset = load_iris()
+_classifier = tree.DecisionTreeClassifier()
 
+# print the list of labels available for this dataset
 print(list(iris_dataset.target_names))
+
+classifier = _classifier.fit(
+  iris_dataset.data,
+  iris_dataset.target
+)
+
+examples = [
+  [5.1, 3.5, 1.4, 0.2],
+  [5.1, 3.5, 1.4, 0.2],
+  [5.1, 3.5, 1.4, 0.2],
+  [5.1, 3.5, 1.4, 0.2],
+  [5.1, 3.5, 1.4, 1.5],
+  [6.5, 3.0, 5.8, 2.2]
+]
+prediction = classifier.predict(examples)
+print(prediction)
+# the indices match the labels in `iris_dataset.target_names`
